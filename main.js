0489198c3cc8a1eeae627a6635ff612d0774f71c
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-05-06 00:22:03 -0600

    photos are appending

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 $(document).ready(function() {
   $('#submit').on("click", function() {
   var zip = $('#zipinput').val();
-  var $xhr = $.getJSON(`https://g-pet.herokuapp.com/pet.find?location=${zip}&animal=dog&count=12&format=json`);
+  var $xhr = $.getJSON(`https://g-pet.herokuapp.com/pet.find?location=${zip}&animal=dog&count=30&format=json`);
   $xhr.done(function(data){
     var dogs = data.petfinder.pets.pet;
+    console.log(dogs.length);
     // console.log(names[0]['name']['$t']);
     // $('#dog1').html(names[0]['name']['$t']);
     console.log(dogs);
     for (var i = 0; i < dogs.length; i++) {
-      let title = '#dog' + (i + 1) + ' .panel-title';
-        $(title).html(dogs[i]['name']['$t']);
-        let bio = '#dog' + (i + 1) + ' .panel-body'
-          $(bio).html(dogs[i]['description']['$t']);
-        let pic = '#dog' + (i + 1) + ' .thumbnail col-md-12'
-          $(pic).html(dogs[i]['media']['photos'][0]);
+      let name = '#dog' + (i + 1) + ' .panel-title';
+        $(name).html(dogs[i].name.$t);
+        let bio = '#dog' + (i + 1) + ' .panel-body';
+          $(bio).html(dogs[i].description.$t);
+          let pic = '#image' + (i + 1);
+          // console.log($('#dog1'));
+          let photoArr = dogs[i].media.photos.photo[0]['$t'];
+          console.log(source);
+            $(pic).attr("src", dogs[i].media.photos.photo[0].$t);
+            // console.log(dogs[0]['media']['photos']['photo'][0]['$t']);
     }
+
     // for(let key of names) {
     //   console.log(key);
     //   $('.panel-title').html(key.name['$t']);
