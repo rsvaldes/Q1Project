d2947a9c887b0e33755e64a449669991ad039083
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-05-08 10:21:49 -0600

    search options and featured dog working

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
-  // var alreadyZip = localStorage.getItem()
+  var $yhr = $.getJSON(`https://g-pet.herokuapp.com/pet.getRandom?animal=dog&count=1&output=basic&format=json`);
+  $yhr.done(function(data) {
+    var featured = data.petfinder.pet;
+    let featuredname = '#featureddog .panel-title';
+    $(featuredname).html(featured.name.$t);
+    let featuredbio = '#featureddog .panel-body';
+    $(featuredbio).html(featured.description.$t);
+    let featuredpic = '#featuredimage';
+    $(featuredpic).attr("src",featured.media.photos.photo[3].$t );
 
+  });
+  // var alreadyZip = localStorage.getItem()
   // if alreadyzip, then fire off getJSON request
   function buildPanels() {
     for (var i = 0; i < 12; i++) {
@@ -25,7 +35,7 @@ function setDogId () {
   for (var i = 0; i < pups.length; i++) {
     count++;
   $(pups[i]).attr('id', "dog" + count);
-}
+  }
 }
 function setImageId () {
   var count = 0;
@@ -33,32 +43,40 @@ function setImageId () {
   for (var i = 0; i < pics.length; i++) {
     count++;
   $(pics[i]).attr('id', "image" + count);
-}
+  }
 }
 
-  $('#submit').on("click", function() {
+
+$('#submit').on("click", function() {
+    $('#featureddog').hide();
+    $('h1').hide();
     buildPanels();
       var zip = $('#zipinput').val();
-      var $xhr = $.getJSON(`https://g-pet.herokuapp.com/pet.find?location=${zip}&animal=dog&count=12&format=json`);
+      var size = $('#sizeselect').val();
+      var age = $('#ageselect').val();
+      var sex = $('#sexselect').val();
+      var url = `https://g-pet.herokuapp.com/pet.find?location=${zip}&animal=dog&count=12&format=json`;
+      if (size) {
+        url += `&size=${size}`;
+      }
+      if (age) {
+        url += `&age=${age}`;
+      }
+      if (sex) {
+        url += `&sex=${sex}`;
+      }
+      var $xhr = $.getJSON(url);
       $xhr.done(function(data){
-        var dogs = data.petfinder.pets.pet;
-        console.log(dogs);
+        let dogs = data.petfinder.pets.pet;
+        console.log(data);
         for (var i = 0; i < dogs.length; i++) {
           let name = '#dog' + (i + 1) + ' .panel-title';
             $(name).html(dogs[i].name.$t);
             let bio = '#dog' + (i + 1) + ' .panel-body';
               $(bio).html(dogs[i].description.$t);
               let pic = '#image' + (i + 1);
-              let photoArr = dogs[i].media.photos.photo
-              [3]['$t'];
-                $(pic).attr("src", dogs[i].media.photos.photo[0].$t);
-      }
-  });
-
+                $(pic).attr("src", dogs[i].media.photos.photo[3].$t);
+              }
 });
-
-})
-// });
-  // }
-// });
-// });
+});
+});
