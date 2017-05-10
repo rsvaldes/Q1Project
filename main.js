a5679e702314961d1254dcf33c7c17ae58b3fd0e
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-05-10 15:12:57 -0600

    making progress with multiple pages

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  var $ihr = $.getJSON(`https://g-ipinfodb.herokuapp.com/v3/ip-city/?format=json`);
-  $ihr.done(function(data) {
-    console.log(data);
-  });
+  // var $ihr = $.getJSON(`https://g-ipinfodb.herokuapp.com/v3/ip-city/?format=json`);
+  // $ihr.done(function(data) {
+  //   console.log(data);
+  // });
   var audio = document.createElement("audio");
   audio.src = "dogbark.m4a";
 $('.cartoons').hover(function() {
@@ -16,7 +16,7 @@ $('.cartoons').hover(function() {
     $(featuredname).html(featured.name.$t);
     let featuredbio = '#featureddog .panel-body';
     let featuredpic = '#featuredimage';
-    $(featuredbio).html('<img class = "images img-responsive thumbnail" id=' + featuredpic + ' src=  ' + featured.media.photos.photo[3].$t + '>' + 'My ID: ' + featured.id.$t + "<br>" + 'My Shelter\'s ID: ' + featured.shelterId.$t + "</br>" + 'Shelter Phone: ' + featured.contact.phone.$t + "<br>" + 'Shelter Email: ' + featured.contact.email.$t + "<br>" + featured.description.$t);
+    $(featuredbio).html('<img class = "images img-responsive thumbnail" id=' + featuredpic + ' src=  ' + featured.media.photos.photo[3].$t + '>' + 'My ID: ' + featured.id.$t + "<br>" + 'My Shelter\'s ID: ' + featured.shelterId.$t + "</br>" + 'Shelter Phone: ' + featured.contact.phone.$t + "<br>" + 'Shelter Email: ' + "<a href = mailto:" + featured.contact.email.$t + ">" + featured.contact.email.$t + "</a>" + "<br>" + featured.description.$t);
   });
   function buildPanels(dogs) {
     for (var i = 0; i < dogs.length; i++) {
@@ -56,8 +56,7 @@ $('#submit').on("click", function() {
       if (zip.length !== 5) {
       alert('Please enter a valid zip code');
       }
-      var alreadyZip = localStorage.setItem('location', zip);
-      console.log(alreadyZip);
+      localStorage.setItem('location', zip);
       var size = $('#sizeselect').val();
       var age = $('#ageselect').val();
       var sex = $('#sexselect').val();
@@ -76,9 +75,8 @@ $('#submit').on("click", function() {
         // console.log(data);
         let dogs = data.petfinder.pets.pet;
         offset = data.petfinder.lastOffset.$t;
-        // console.log(response);
-        // console.log(pet);
-        // console.log(dogs);
+        var newurl = url += '&offset=' + offset;
+        console.log(newurl);
         buildPanels(dogs);
         $('#navigate').show();
         for (var i = 0; i < dogs.length; i++) {
@@ -86,28 +84,23 @@ $('#submit').on("click", function() {
           $(name).html(dogs[i].name.$t);
           let bio = '#dog' + (i + 1) + ' .panel-body';
           let pic = 'image' + (i + 1);
-          $(bio).html('<img class = "images img-responsive thumbnail" id=' + pic + ' src=  ' + dogs[i].media.photos.photo[3].$t + '>' + 'My ID: ' + dogs[i].id.$t + "<br>" + 'My Shelter\'s ID: ' + dogs[i].shelterId.$t + "</br>" + 'Shelter Phone: ' + dogs[i].contact.phone.$t + "<br>" + 'Shelter Email: ' + dogs[i].contact.email.$t + "<br>" + dogs[i].description.$t);
+          $(bio).html('<img class = "images img-responsive thumbnail" id=' + pic + ' src=  ' + dogs[i].media.photos.photo[3].$t + '>' + "<b>"+' My ID: ' + dogs[i].id.$t + "<br>" + 'My Shelter\'s ID: ' + dogs[i].shelterId.$t + "</br>" + 'Shelter Phone: ' + dogs[i].contact.phone.$t + "<br>" + 'Shelter Email: ' + "</b>" + "<a href = mailto:" + dogs[i].contact.email.$t + ">" + dogs[i].contact.email.$t +"</a>" + "<br>" + dogs[i].description.$t);
         }
-        function getMore (data) {
-          $('.moreresults').on("click", function () {
-            buildPanels(dogs);
-            $('#navigate').show();
-          });
-          localStorage.getItem(alreadyZip);
-          var $brh = `https://g-pet.herokuapp.com/pet.find?location=${alreadyZip}&animal=dog&count=12&format=json&offset=${offset}`;
-          $bhr.done(function (data) {
-            for (var i = 0; i < dogs.length; i++) {
-              let name = '#dog' + (i + 1) + ' .panel-title';
-              $(name).html(dogs[i].name.$t);
-              let bio = '#dog' + (i + 1) + ' .panel-body';
-              let pic = 'image' + (i + 1);
-              $(bio).html('<img class = "images img-responsive thumbnail" id=' + pic + ' src=  ' + dogs[i].media.photos.photo[3].$t + '>' + 'My ID: ' + dogs[i].id.$t + "<br>" + 'My Shelter\'s ID: ' + dogs[i].shelterId.$t + "</br>" + 'Shelter Phone: ' + dogs[i].contact.phone.$t + "<br>" + 'Shelter Email: ' + dogs[i].contact.email.$t + "<br>" + dogs[i].description.$t);
-            }
-          });
-        }
-        getMore(data);
+        
+      });
 
-});
+          $('#moreresults').on("click", function (newurl) {
+          var alreadyZip = localStorage.getItem('location');
+          console.log(alreadyZip);
+          console.log(newurl);
+          var $bhr = $.getJSON(newurl);
+          $bhr.done(function (data) {
+          console.log(data);
+
+          });
+        }
+);
+
 });
 function buildRows (data) {
   for (var i = 0; i < data.length; i++)  {
@@ -132,21 +125,27 @@ function setRowId () {
     $(pounds[i]).attr('id', "shelter" + (i + 1));
   }
 }
+var clicked = false;
 $('#search').on("click", function () {
+  clicked = true;
+  if (clicked === true) {
+    $('#shelterlist').html('');
+  }
   $("#labels").show();
   var shelterZip = $('#shelterinput').val();
-  var $zhr = $.getJSON(`https://g-pet.herokuapp.com/shelter.find?location=${shelterZip}&count=12&format=json`);
+  var $zhr = $.getJSON(`https://g-pet.herokuapp.com/shelter.find?location=${shelterZip}&count=20&format=json`);
   $zhr.done(function(data){
     let rescues = data.petfinder.shelters.shelter;
     console.log('rescues', rescues);
     buildRows(rescues);
     buildDivs();
     for (let i = 0; i < rescues.length; i++) {
+    // let emailaddress = rescues[i].email.$t;
     let poundName = '#shelter' + (i + 1) + ' .name';
     let poundEmail = '#shelter' + (i + 1) + ' .email';
     let poundPhone = '#shelter' + (i + 1) + ' .phone';
     $(poundName).html(rescues[i].name.$t);
-    $(poundEmail).html(rescues[i].email.$t);
+    $(poundEmail).html("<a href = mailto:" + rescues[i].email.$t + ">" +rescues[i].email.$t +"</a>" );
     $(poundPhone).html(rescues[i].phone.$t);
   }
 });
